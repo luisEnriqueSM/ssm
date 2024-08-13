73c7136e75531b79d3d342289ad6205ed42a0e73
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-13 12:55:35 -0600

    Initial commit

---
+++ src/test/java/org/beer/works/ssm/SsmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.beer.works.ssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
