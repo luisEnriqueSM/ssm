6f1ec2ccf2c115dc4bb44b71d5237fcec937ce1e
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-14 10:21:46 -0600

    Create test - Progressing state from New to Pre-Auth

---
+++ src/test/java/org/beer/works/ssm/services/PaymentServiceImplTest.java
@@ -0,0 +1,49 @@
+package org.beer.works.ssm.services;
+
+import org.beer.works.ssm.domain.Payment;
+import org.beer.works.ssm.domain.PaymentEvent;
+import org.beer.works.ssm.domain.PaymentState;
+import org.beer.works.ssm.repository.PaymentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PaymentServiceImplTest {
+
+    @Autowired
+    PaymentService paymentService;
+    @Autowired
+    PaymentRepository paymentRepository;
+    Payment payment;
+
+    @BeforeEach
+    void setUp() {
+        payment = Payment.builder()
+                .amount(new BigDecimal("12.99"))
+                .build();
+    }
+
+    @Transactional
+    @Test
+    void preAuth() {
+        Payment savedPayment = paymentService.newPayment(payment);
+
+        System.out.println("Before: " + savedPayment);
+
+        StateMachine<PaymentState, PaymentEvent> sm = paymentService.preAuth(savedPayment.getId());
+
+        Payment preAuthedPayment = paymentRepository.getReferenceById(savedPayment.getId());
+
+        System.out.println(sm.getState().getId());
+
+        System.out.println("After: " + preAuthedPayment);
+    }
+}
