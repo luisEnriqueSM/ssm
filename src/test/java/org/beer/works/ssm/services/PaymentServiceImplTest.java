a401dc6a7ac98e38f4ace5d1a7ce3ba4630ae78a
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-14 12:52:40 -0600

    Progess State from Pre-Auth to Auth

--- src/test/java/org/beer/works/ssm/services/PaymentServiceImplTest.java
+++ src/test/java/org/beer/works/ssm/services/PaymentServiceImplTest.java
@@ -5,6 +5,7 @@ import org.beer.works.ssm.domain.PaymentEvent;
 import org.beer.works.ssm.domain.PaymentState;
 import org.beer.works.ssm.repository.PaymentRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -45,5 +46,24 @@ class PaymentServiceImplTest {
         System.out.println(sm.getState().getId());
 
         System.out.println("After: " + preAuthedPayment);
+    }
+
+    @Transactional
+    @RepeatedTest(10)
+    //@Test
+    void testAuth() {
+        Payment savedPayment = paymentService.newPayment(payment);
+
+        StateMachine<PaymentState, PaymentEvent> preAuthSM = paymentService.preAuth(savedPayment.getId());
+
+        if (preAuthSM.getState().getId() == PaymentState.PRE_AUTH) {
+            System.out.println("Payment is Pre Authorized");
+
+            StateMachine<PaymentState, PaymentEvent> authSM = paymentService.authorizePayment(savedPayment.getId());
+
+            System.out.println("Result of Auth: " + authSM.getState().getId());
+        } else {
+            System.out.println("Payment failed pre-auth...");
+        }
     }
 }
